fdbf6357d070952a2484977541f096e213db31be
Ramesh Allapuram <ramesh@Silvotron>
2022-08-25 21:58:51 +0530

    Assignment file added

---
+++ Assignments/Assignment1_BasicProblems/src/OddEven.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int num = in.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("Entered number is even.");
+        } else {
+            System.out.println("Entered number is odd.");
+        }
+
+    }
+}
